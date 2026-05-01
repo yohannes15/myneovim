acda021a8bb1de8b7ecb43293455c478a4708e77
Yohannes <y.berhane56@gmail.com>
2026-04-30 22:32:39 -0400

    Removed and commented

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,16 +5,15 @@ return {
     lazy = false,
     ---@type snacks.Config
     opts = {
-      terminal = { enabled = true },
-      indent = { enabled = true },
-      input = { enabled = true },
-      notifier = { enabled = true },
-      scope = { enabled = true },
-      words = { enabled = true },
-      statuscolumn = { enabled = true },
-      bigfile = { enabled = true },
-      quickfix = { enabled = true },
-      picker = { enabled = true },
+      terminal = { enabled = true }, --split/floating terminal
+      indent = { enabled = true }, -- indent guides and scopes
+      input = { enabled = true }, -- better input
+      notifier = { enabled = true }, -- prettier notifier
+      scope = { enabled = true }, -- scope highlighting / jump / visual 
+      words = { enabled = true }, -- word highlighting and jump word refs
+      statuscolumn = { enabled = true }, -- pretty status column
+      bigfile = { enabled = true }, -- deal with big files
+      picker = { enabled = true }, -- selecting items
     },
     keys = {
       -- Terminal
@@ -26,6 +25,10 @@ return {
       -- Git
       { "<leader>gb", function() Snacks.gitbrowse() end, desc = "[G]it [B]rowse" },
       { "<leader>gl", function() Snacks.picker.git_log() end, desc = "[G]it [L]og" },
+
+      -- Words
+      { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+      { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
     },
   },
 }
